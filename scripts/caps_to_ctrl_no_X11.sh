be4d60530921e3b75fe26e2682645f99b81cb719
djon3s <hack@riseup.net>
2014-04-22 22:37:53 +1000

    never get stuck without a ctrl on caps again

---
+++ scripts/caps_to_ctrl_no_X11.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Converts caps to a control key without needing X etc
+# useful when you've borked a kernel install and get
+# dropped into a virtual terminal or for spiritually
+# pure aesthetes who worship the austere divinities of 
+# text and pipe. 
+sudo dumpkeys | head -1 > ~/.keymap_caps2ctrl_no_X
+echo "keycode 58 = Control" >> ~/.keymap_caps2ctrl_no_X
+sudo loadkeys ~/.keymap_caps2ctrl_no_X
+
+# to unload, sudo loadkeys -d
