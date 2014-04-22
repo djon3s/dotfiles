fd71eb79837e3c4c9b318800f5ff3473fe74a85c
djon3s <hack@riseup.net>
2014-04-22 22:49:45 +1000

    distribution independent (i think)

--- scripts/caps_to_ctrl_no_X11.sh
+++ scripts/caps_to_ctrl_no_X11.sh
@@ -4,10 +4,12 @@
 # dropped into a virtual terminal or for spiritually
 # pure aesthetes who worship the austere divinities of 
 # text and pipe.
+#
+# Aims to be distribution independent, let me know if 
+# it breaks that rule.
 sudo dumpkeys | head -1 > ~/.keymap_caps2ctrl_no_X
 echo "keycode 58 = Control" >> ~/.keymap_caps2ctrl_no_X
 sudo loadkeys ~/.keymap_caps2ctrl_no_X
 
 # to unload, sudo loadkeys -d
-
 # TODO: make this an init script 
